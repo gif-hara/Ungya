90fbae61a6fc8c4353a77e0ba60b414ac4f45960
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-10-24 00:52:58 +0900

    :new: フッターに分ける

---
+++ Assets/HK/Ungya/Scripts/UI/FooterController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using HK.Framework.EventSystems;
+using HK.Framework.Text;
+using HK.Ungya.Events.UI;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HK.Ungya.UI
+{
+    public sealed class FooterController : MonoBehaviour
+    {
+        [SerializeField]
+        private List<FooterButton> footerButtons;
+
+        private List<Action> buttonActions = new List<Action>();
+
+        [SerializeField]
+        private StringAsset.Finder skillName;
+
+        [SerializeField]
+        private StringAsset.Finder exerciseName;
+
+        [SerializeField]
+        private StringAsset.Finder sortName;
+        
+        [SerializeField]
+        private StringAsset.Finder cancelName;
+
+        void Awake()
+        {
+            for (int i = 0; i < this.footerButtons.Count; i++)
+            {
+                this.buttonActions.Add(null);
+
+                var footerButton = this.footerButtons[i];
+                footerButton.Button.OnClickAsObservable()
+                    .Where(_ => footerButton.Button.isActiveAndEnabled)
+                    .SubscribeWithState2(this, i, (_, _this, _i) =>
+                    {
+                        var action = _this.buttonActions[_i];
+                        if (action != null)
+                        {
+                            action();
+                        }
+                    })
+                    .AddTo(this);
+            }
+
+            UniRxEvent.GlobalBroker.Receive<ChangeUI>()
+                .SubscribeWithState(this, (c, _this) =>
+                {
+                    if (c.UIType == UIType.Main)
+                    {
+                        _this.ToMain();
+                    }
+                    else if (c.UIType == UIType.Exercise)
+                    {
+                        _this.ToExercise();
+                    }
+                })
+                .AddTo(this);
+        }
+
+        public void Setup(params ButtonParameter[] parameters)
+        {
+            foreach (var footerButton in this.footerButtons)
+            {
+                footerButton.Button.gameObject.SetActive(false);
+            }
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                var buttonParameter = parameters[i];
+                var footerButton = this.footerButtons[i];
+                this.buttonActions[i] = buttonParameter.Action;
+                footerButton.Text.text = buttonParameter.Name.Get;
+                footerButton.Button.gameObject.SetActive(true);
+            }
+        }
+        
+        private void ToMain()
+        {
+            this.Setup(
+                this.CreateSkillParameter,
+                this.CreateExerciseParameter
+                );
+        }
+
+        private void ToExercise()
+        {
+            this.Setup(
+                new ButtonParameter(this.sortName, () =>
+                {
+                    Debug.Log("TODO");
+                }),
+                new ButtonParameter(this.cancelName, () =>
+                {
+                    UniRxEvent.GlobalBroker.Publish(ChangeUI.GetCache(UIType.Main));
+                })
+                );
+        }
+
+        private ButtonParameter CreateExerciseParameter
+        {
+            get
+            {
+                return new ButtonParameter(this.exerciseName, () =>
+                {
+                    UniRxEvent.GlobalBroker.Publish(ChangeUI.GetCache(UIType.Exercise));
+                });
+            }
+        }
+
+        private ButtonParameter CreateSkillParameter
+        {
+            get
+            {
+                return new ButtonParameter(this.skillName, () =>
+                {
+                    Debug.Log("TODO");
+                });
+            }
+        }
+        
+        [Serializable]
+        public class FooterButton
+        {
+            public Button Button;
+
+            public Text Text;
+        }
+
+        public class ButtonParameter
+        {
+            public StringAsset.Finder Name;
+
+            public Action Action;
+
+            public ButtonParameter(StringAsset.Finder name, Action action)
+            {
+                this.Name = name;
+                this.Action = action;
+            }
+        }
+    }
+}
--- Assets/HK/Ungya/Scripts/UI/UIManager.cs
+++ Assets/HK/Ungya/Scripts/UI/UIManager.cs
@@ -1,13 +1,12 @@
 ﻿using DG.Tweening;
+using HK.Framework.EventSystems;
+using HK.Ungya.Events.UI;
 using UnityEngine;
 
 namespace HK.Ungya.UI
 {
     public sealed class UIManager : MonoBehaviour
     {
-        [SerializeField]
-        private GameObject main;
-
         [SerializeField]
         private float tweenDutation;
 
@@ -17,5 +16,11 @@ namespace HK.Ungya.UI
         public float TweenDutation { get { return this.tweenDutation; } }
 
         public Ease TweenEase { get { return this.tweenEase; } }
+
+        void Start()
+        {
+            // 最初のUIタイプを発行する
+            UniRxEvent.GlobalBroker.Publish(ChangeUI.GetCache(UIType.Main));
+        }
     }
 }
