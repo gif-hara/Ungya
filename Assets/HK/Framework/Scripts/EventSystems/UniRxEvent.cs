432d4779bef22bea3a8329138400176d200a6a72
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-10-23 16:13:12 +0900

    :volcano: HK.Framework

--- Assets/HK/Framework/Scripts/EventSystems/UniRxEvent.cs
+++ Assets/HK/Framework/Scripts/EventSystems/UniRxEvent.cs
@@ -13,11 +13,27 @@ namespace HK.Framework.EventSystems
     public abstract class UniRxEvent<E> : UniRxEvent
         where E : UniRxEvent<E>, new()
     {
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
         public static E Get()
         {
             var result = new E();
 
             return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache()
+        {
+            return cache;
         }
     }
 
@@ -26,12 +42,29 @@ namespace HK.Framework.EventSystems
     {
         protected P1 param1;
 
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
         public static E Get(P1 param1)
         {
             var result = new E();
             result.param1 = param1;
 
             return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache(P1 param1)
+        {
+            cache.param1 = param1;
+            return cache;
         }
     }
 
@@ -42,6 +75,9 @@ namespace HK.Framework.EventSystems
 
         protected P2 param2;
 
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
         public static E Get(P1 param1, P2 param2)
         {
             var result = new E();
@@ -49,6 +85,21 @@ namespace HK.Framework.EventSystems
             result.param2 = param2;
 
             return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache(P1 param1, P2 param2)
+        {
+            cache.param1 = param1;
+            cache.param2 = param2;
+            return cache;
         }
     }
 
@@ -61,6 +112,9 @@ namespace HK.Framework.EventSystems
 
         protected P3 param3;
 
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
         public static E Get(P1 param1, P2 param2, P3 param3)
         {
             var result = new E();
@@ -69,6 +123,22 @@ namespace HK.Framework.EventSystems
             result.param3 = param3;
 
             return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache(P1 param1, P2 param2, P3 param3)
+        {
+            cache.param1 = param1;
+            cache.param2 = param2;
+            cache.param3 = param3;
+            return cache;
         }
     }
 
@@ -83,6 +153,9 @@ namespace HK.Framework.EventSystems
 
         protected P4 param4;
 
+        /// <summary>
+        /// イベントを取得します
+        /// </summary>
         public static E Get(P1 param1, P2 param2, P3 param3, P4 param4)
         {
             var result = new E();
@@ -92,6 +165,23 @@ namespace HK.Framework.EventSystems
             result.param4 = param4;
 
             return result;
+        }
+        
+        protected static E cache = new E();
+
+        /// <summary>
+        /// キャッシュイベントを取得します
+        /// </summary>
+        /// <remarks>
+        /// キャッシュを利用しているため<c>new</c>はしませんがスレッドセーフではありません
+        /// </remarks>
+        public static E GetCache(P1 param1, P2 param2, P3 param3, P4 param4)
+        {
+            cache.param1 = param1;
+            cache.param2 = param2;
+            cache.param3 = param3;
+            cache.param4 = param4;
+            return cache;
         }
     }
 }
