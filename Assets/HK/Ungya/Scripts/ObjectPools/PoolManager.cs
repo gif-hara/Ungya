c27e0c9b7d3418caebe51ce2bfcca0037fc18b86
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-10-26 01:19:39 +0900

    :new: PoolManager作ってみる

---
+++ Assets/HK/Ungya/Scripts/ObjectPools/PoolManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UniRx.Toolkit;
+using UnityEngine;
+
+namespace HK.Ungya.ObjectPools
+{
+    public static class PoolManager
+    {
+        public static readonly Dictionary<Component, ComponentPool> pools = new Dictionary<Component, ComponentPool>();
+
+        public static ComponentPool GetPool(this Component component)
+        {
+            ComponentPool pool;
+            if (!pools.TryGetValue(component, out pool))
+            {
+                pool = new ComponentPool(component);
+                pools.Add(component, pool);
+            }
+
+            return pool;
+        }
+
+        public class ComponentPool : ObjectPool<Component>
+        {
+            private readonly Component original;
+            
+            public ComponentPool(Component original)
+            {
+                this.original = original;
+            }
+            
+            protected override Component CreateInstance()
+            {
+                return Object.Instantiate(this.original);
+            }
+        }
+    }
+}
